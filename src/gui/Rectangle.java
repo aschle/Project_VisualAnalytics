8f51017e24448399dc1ed33787ec1408e642bec5
Alexa Schlegel <alexa.schlegel@gmail.com>
2011-07-28 00:43:31 +0200

    MouseOver Funktion hinzugefügt

--- src/gui/Rectangle.java
+++ src/gui/Rectangle.java
@@ -5,21 +5,29 @@ import squarification.RectInterface;
 
 public class Rectangle implements RectInterface {
 
-	private int area;
+	private double area;
 	private int dimX;
 	private int dimY;
 	private int startX;
 	private int startY;
+	private String title;
+	
+	private int offsetX;
+	private int offsetY;
+	private static int margin = 2;
 
 	PApplet parent;
 
-	public Rectangle(int area, PApplet parent) {
+	public Rectangle(double area, String title, PApplet parent) {
 		this.area = area;
 		this.parent = parent;
+		this.title = title;
+		this.offsetX = 0;
+		this.offsetY = 0;
 	}
 
 	@Override
-	public int getArea() {
+	public double getArea() {
 		return area;
 	}
 
@@ -34,9 +42,27 @@ public class Rectangle implements RectInterface {
 		this.startX = startX;
 		this.startY = startY;
 	}
+	
+	public void setOffset(int offsetX, int offsetY){
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
+		startX += offsetX;
+		startY += offsetY;
+	}
 
 	public void display() {
 		parent.fill(200);
-		parent.rect(startX, startY, dimX, dimY);
+		parent.rect(startX + margin, startY + margin, dimX - 2 * margin, dimY
+				- 2 * margin);
+	}
+
+	public void mouseText() {
+		if (parent.mouseX > startX && parent.mouseX < startX + dimX
+				&& parent.mouseY > startY && parent.mouseY < startY + dimY) {
+			parent.fill(200, 200, 200, 150);
+			parent.rect(parent.mouseX - 10, parent.mouseY - 20, 150, 30);
+			parent.fill(0);
+			parent.text(title, parent.mouseX, parent.mouseY);
+		}
 	}
 }
